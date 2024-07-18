779b8615b1323beecadab62e92c05137d0b1e8fe
haruka.suda <suda@revcreate.co.jp>
2024-07-18 17:02:47 +0900

    Inertia-vue.js

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue';
+
+const newTitle = ref('');
+const newContent = ref('');
+</script>
+
+<template>
+    <div class="m-8">
+        <input type="text" name="newTitle" v-model="newTitle" />{{ newTitle }}<br />
+        <input type="text" name="newContent" v-model="newContent" />{{ newContent
+        }}<br />
+        <Link
+            as="button"
+            method="post"
+            :href="route('inertia.store')"
+            :data="{ title: newTitle, content: newContent }">保存テスト
+        </Link>
+    </div>
+</template>
